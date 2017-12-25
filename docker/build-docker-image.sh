0831fd3d9eb77cdec09b1fcf1d90c476d8d1e042
HayesZhong <397721763@qq.com>
2017-12-25 21:30:09 +0800

    add Dockerfile

--- docker/build-docker-image.sh
+++ docker/build-docker-image.sh
@@ -4,8 +4,8 @@ cd ..
 mvn clean package install -DskipTests
 
 IMAGE_VERSION="0.1"
-docker build -t registry.cn-hangzhou.aliyuncs.com/stonk/spark-k8s-driver:${IMAGE_VERSION} driver/Dockerfile
+docker build -t registry.cn-hangzhou.aliyuncs.com/stonk/spark-k8s-driver:${IMAGE_VERSION} docker/driver/Dockerfile
 docker push registry.cn-hangzhou.aliyuncs.com/stonk/spark-k8s-driver:${IMAGE_VERSION}
 
-docker build -t registry.cn-hangzhou.aliyuncs.com/stonk/spark-k8s-excutor:${IMAGE_VERSION} driver/Dockerfile
-docker push registry.cn-hangzhou.aliyuncs.com/stonk/spark-k8s-excutor:${IMAGE_VERSION}
+docker build -t registry.cn-hangzhou.aliyuncs.com/stonk/spark-k8s-executor:${IMAGE_VERSION} docker/executor/Dockerfile
+docker push registry.cn-hangzhou.aliyuncs.com/stonk/spark-k8s-executor:${IMAGE_VERSION}
